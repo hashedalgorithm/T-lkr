f46888baea6f95c26b884d51a95ba768a5bddc7a
Sanjay Kumar Kumaravelan <sanjaykumarcena@gmail.com>
2026-01-14 23:46:46 +0100

    feat: impls basic subtitle container and text

--- background.js
+++ background.js
@@ -1,6 +1,1 @@
 console.log("Worker loaded");
-
-chrome.action.onClicked.addListener((tab) => {
-  console.log(tab.title);
-  console.log("Hellooo");
-});
--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,11 @@
-(function () {
-  console.log("hello world!");
-})();
+const subtitleText = document.createElement("p");
+const subtitleContainer = document.createElement("div");
+
+subtitleContainer.classList.add("TULKR_container");
+subtitleText.classList.add("TULKR_text");
+
+subtitleContainer.appendChild(subtitleText);
+
+subtitleText.innerText = "HELLO this is my sample subtitle!";
+
+document.body.append(subtitleContainer);
